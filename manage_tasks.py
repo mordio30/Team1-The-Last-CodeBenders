e4e678b7b1b1894152cc5cb152609d612aae6f30
YOUR_NAME <YOUR_EMAIL>
2025-02-07 18:36:54 -0600

    delete task roughly completed

--- manage_tasks.py
+++ manage_tasks.py
@@ -1,5 +1,7 @@
 # Make a program to add, delete, update and read tasks
-tasks = []
+tasks = [
+
+]
 priority_level =['high', 'medium', 'low']
 days = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday']
 
@@ -9,13 +11,32 @@ def view_tasks():
 def add_task():
     task = {
         'taskname': input("Enter the task name: ").lower(), # Must be a string
-        'taskuser': input("Who is responsible for this task:"), # Must be a string
+        'taskuser': input("Who is responsible for this task: "), # Must be a string
         'taskdeadline': input("What day should the task be finished by: ").lower(), # Must be a day & converted to all lowercase
         'prioritylevel': input("Please select High, Medium, or Low priority: ").lower(), # Must be a defined priority & converted to all lowercase
     }
     tasks.append(task)
 
-# def delete_task():
+def delete_task():
+    task_to_delete = input("Which task do you want to delete?: ").lower() # The task the user wants to delete
+    # iterates through the tasks list of dicts
+    for task in tasks:
+        # print(task)
+        # if task_to_delete_value matches i['taskname']           
+        if task['taskname'] == task_to_delete:
+            tasks.remove(task)
+            # new_task_list = (list(filter(lambda)))
+            # tasks = new_task_list
+        else: print(f" { task_to_delete } does not exist in tasks ")
+        
+    
+
+       
+            
+
 
 add_task()
+add_task()
+view_tasks()
+delete_task()
 view_tasks()
