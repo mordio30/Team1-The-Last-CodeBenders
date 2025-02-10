336df83ca8608e4b4671b2f3a37cfb7a4b2f5fff
jack-himlo <devon.g.jackson417@gmail.com>
2025-02-10 17:07:22 -0500

    WIP checking for multiples in contactSearch

--- contacts.py
+++ contacts.py
@@ -10,66 +10,87 @@ def showContacts():
     else:
         print("No contacts available.")
 
-print(f" {showContacts()}")
+#print(f" {showContacts()}")
 #add contacts
 def addContact():
-    firstName =(input("contacts first name: "))
-    lastName = (input("contacts last name: "))
+    firstName =(input("Enter new contacts first name: "))
+    lastName = (input("Enter new contacts last name: "))
     fullName = firstName + ' ' + lastName
-    contactNumber = (input("contact number: "))
-    contactEmail = (input("contacts email: "))
+    contactNumber = (input("Enter new contact number: "))
+    contactEmail = (input("Enter new contacts email: "))
     newContact = {'name': fullName, 'number' : contactNumber, 'email' : contactEmail}
 
     contacts.append(newContact)
+    # print(f"you have added {newContact} to your Contacts")
     
 
-    return newContact
+    return f"you have added {newContact} to your Contacts"
 
-print(f" {addContact()}")
-print(f" {showContacts()}")
+#print(f" {addContact()}")
+#print(f" {showContacts()}")
 
 #contact search
 
 def contactSearch():
     print(f"{contacts}")
-    search = input("enter the contacts name: ").lower()
-    found_contact = None
+    search = input("enter the contacts name you would like to search for: ").lower()
+    found_contact = []
 
     for contact in contacts: 
         if search in contact['name'].lower():
-            found_contact = contact
-            break
+            found_contact.appened(contact)
     if found_contact:
-        print(f"Contact found :")
-        print(f"Name: {found_contact['name']}, Number: {found_contact['number']}, Email: {found_contact['email']}")
-                
+            print(f"Contact found from search:")
+            print(f"Name: {found_contact['name']}, Number: {found_contact['number']}, Email: {found_contact['email']}")
+           for i, contact in enumerate(found_contact):
+                print(f"{i}: Name: {contact['name']}, Email: {contact['email']}, Number: {contact['number']}")     
 
     else:
-        print(f"No contact was found with that information")
+        print(f"No contact was found with that search information")
 
 
-contactSearch()        
+#contactSearch()        
 
 
 def contactDelete():
-    print(f"{contacts}")
-    search = input("enter the contacts name: ").lower()
+    global contacts
+    search = input("enter the contacts name you want to be deleted: ").lower()
     toDelete = None
-
+    duplicate_contacts = []
+    print(contacts)
+   
+        
     for contact in contacts: 
         if search in contact['name'].lower():
-            toDelete = contact
-            break
-    if toDelete:
-        contacts.remove(toDelete)
-        print(f"Contact deleted:")
-        print(f"Name: {toDelete['name']}, Number: {toDelete['number']}, Email: {toDelete['email']}")
-                
-
+            duplicate_contacts.append(contact)
+    if not duplicate_contacts:
+        noDupes = "There are no duplicate contacts"
+        return noDupes
+    if len(duplicate_contacts) > 1:
+        print(f"Multiple contacts found:")
+        for i, contact in enumerate(duplicate_contacts):
+            print(f"{i}: Name: {contact['name']}, Email: {contact['email']}, Number: {contact['number']}")
+        try: 
+            choice= int(input("Enter the number of the contact you want to delete: "))
+            if choice < 0 or choice >= len(duplicate_contacts):
+                print("Nothing was deleted due to invalid input")
+                return
+            toDelete = duplicate_contacts[choice]
+        except ValueError:
+            print("Invalid input, Please enter a number.")
+            return
     else:
-        print(f"No contact was found with that information")
+        toDelete = duplicate_contacts[0]
+    contacts.remove(toDelete)
+    return f"Contact deleted: {toDelete['name']} ({toDelete['number']})"
+
+                    
 
 
-contactDelete() 
-contactSearch()        
-
+print(addContact())
+print(addContact())
+print(showContacts())
+#print(addContact())
+print(contactDelete()) 
+print(contactSearch())        
+print(showContacts())
